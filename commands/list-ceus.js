dea439d7ae7cec1cc1b35a8f8b9aea828355b562
Diogo <davidiog@f45c899af327.ant.amazon.com>
2017-04-18 13:50:14 +1000

    fixing tabs for list ceus

--- commands/list-ceus.js
+++ commands/list-ceus.js
@@ -28,25 +28,29 @@ exports.handler = (event, context, callback) => {
         //Get the total of ceus found
         number = Object.keys(data.Items).length;
 
+        console.log("tot number is ", number);
+
         var response = "```";
 
-        response = response.concat("\n").concat("-".repeat(65)).concat("\n");
-        response = response.concat("| Name").concat(" ".repeat(24)).concat("| CEU_POINTS").concat(" ".repeat(26)).concat("|\n")
-        response = response.concat("-".repeat(65));
-        
+        response = response.concat("\n").concat("-".repeat(62)).concat("\n");
+        response = response.concat("| Name (ceu_points) (hezuo_points)").concat(" ".repeat(27)).concat("|\n");
+        response = response.concat("-".repeat(62));
+                
         // print all members
         data.Items.forEach(function(ceu){
-           response = response.concat("\n| ")
-                              .concat(ceu.ceuname)
-                              .concat(" ".repeat(30 - ceu.ceuname.length))
-                              .concat("| ")
-                              .concat(ceu.hezuo_points)
-                              .concat(" ".repeat(30 - ceu.hezuo_points.length))
-                              .concat("|");
+            
+            response = response.concat("\n| ")
+                               .concat(ceu.ceuname)
+                               .concat(" ".repeat(50 - ceu.ceuname.length))
+                               .concat(" (")
+                               .concat(ceu.ceu_points)
+                               .concat(") (")
+                               .concat(ceu.hezuo_points)
+                               .concat(") |");
         });
 
         response = response.concat("\n")
-                           .concat("-".repeat(65) + "\n")
+                           .concat("-".repeat(62) + "\n")
                            .concat("items found: ".concat(number.toString()))
                            .concat("```");
         
